a6bf83c8799f067fc6988a00e71fa5335ceff3e1
Mehmet Can KALABAŞ <mehmetcankalabas.mk@gmail.com>
2023-04-22 16:38:49 +0300

    AdminPanelCompleted.
    Admin, author and user roles have been determined. Access to unauthorized pages is prohibited. Admin panel completed.

---
+++ Project development with Asp.Net language/Core_Proje/BusinessLayer/Concrete/WriterMessageManager.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterMessageManager : IWriterMessageService
+    {
+        IWriterMessageDal writerMessageDal;
+
+        public WriterMessageManager(IWriterMessageDal writerMessageDal)
+        {
+            this.writerMessageDal = writerMessageDal;
+        }
+
+        public List<WriterMessage> GetListReceiverMessage(string p)
+        {
+            return writerMessageDal.GetbyFilter(x => x.Receiver == p);
+        }
+
+        public List<WriterMessage> GetListSenderMessage(string p)
+        {
+            return writerMessageDal.GetbyFilter(x => x.Sender == p);
+        }
+
+        public void TAdd(WriterMessage t)
+        {
+            writerMessageDal.Insert(t);
+        }
+
+        public void TDelete(WriterMessage t)
+        {
+            writerMessageDal.Delete(t);
+        }
+
+        public WriterMessage TGetByID(int id)
+        {
+            return writerMessageDal.GetByID(id);
+        }
+
+        public List<WriterMessage> TGetList()
+        {
+            return writerMessageDal.GetList();
+        }   
+
+        public List<WriterMessage> TGetListbyFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project development with Asp.Net language/Core_Proje/Core_Proje/Areas/Writer/Controllers/DashBoardWriterController.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashBoardWriterController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashBoardWriterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+
+            //Weather Api
+            string api = "081e86aa2e82bfdfa951c8829a4921e8";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=izmir&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //statistics
+            Context c = new Context();
+            ViewBag.v1 = c.WriterMessages.Where(x => x.Receiver == values.Email).Count();
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = c.Users.Count();
+            ViewBag.v4 = c.Skills.Count();
+
+            return View();
+        }
+    }
+}
